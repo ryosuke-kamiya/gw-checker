1e47792c506b3c5cc658fc285821e39634b53394
KamiyaRyosuke <cinesuija@gmail.com>
2023-09-22 11:49:09 +0900

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,3 +4,22 @@ import Home from '@/layouts/Home/Home';
 export default function RootPage() {
   return <Home />;
 }
+// import { sql } from "@vercel/postgres";
+
+// export default async function Cart({
+//   params
+// } : {
+//   params: { user: string }
+// }): Promise<JSX.Element> {
+//   const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
+
+//   return (
+//     <div>
+//       {rows.map((row) => (
+//         <div key={row.id}>
+//           {row.id} - {row.quantity}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
